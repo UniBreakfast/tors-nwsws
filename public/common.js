49084374e8afa1c193a54abf76874586518f1005
unibreakfast <unibreakfast1@gmail.com>
2020-11-20 23:23:34 +0200

    sounds

--- public/common.js
+++ public/common.js
@@ -1,14 +1,23 @@
-const fixTitle = () => document.title = 'Naive Users '+document.title
+const {assign} = Object
 let body
 
+const btnSound = new Audio('button.mp3')
+const punchSound = new Audio('punch.mp3')
+punchSound.volume = .03
 
-onload = () => {
+const fixTitle = () => document.title = 'Naive Users '+document.title
+
+EventTarget.prototype.on = addEventListener
+
+
+on('load', () => {
   ({body} = document)
   if (localStorage.darkTheme) body.classList.add('dark-theme')
   body.hidden = false
-}
 
-onkeydown = ({code, ctrlKey, altKey, shiftKey}) => {
+})
+
+on('keydown', ({code, ctrlKey, altKey, shiftKey}) => {
   if (ctrlKey && altKey && code=='KeyD') {
     body.classList.toggle('dark-theme')
     if (body.classList.contains('dark-theme')) {
@@ -17,4 +26,22 @@ onkeydown = ({code, ctrlKey, altKey, shiftKey}) => {
       delete localStorage.darkTheme
     }
   }
-}
+})
+
+on('click', ({target: {tagName}}) => {
+  if (tagName=='BUTTON') {
+    btnSound.currentTime = 0
+    btnSound.play()
+  }
+})
+
+let mousedownTime
+
+on('mousedown', ({timeStamp}) => mousedownTime = timeStamp)
+
+on('mouseup', ({target: {tagName}, timeStamp}) => {
+  if (tagName!='BUTTON' && timeStamp-mousedownTime<200) {
+    punchSound.currentTime = 0
+    punchSound.play()
+  }
+})
