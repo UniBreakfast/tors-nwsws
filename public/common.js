dd0867eb577aab14480b5adb062e033c7c969661
unibreakfast <unibreakfast1@gmail.com>
2020-11-20 21:13:00 +0200

    dark-theme

--- public/common.js
+++ public/common.js
@@ -4,5 +4,17 @@ let body
 
 onload = () => {
   ({body} = document)
+  if (localStorage.darkTheme) body.classList.add('dark-theme')
   body.hidden = false
+}
+
+onkeydown = ({code, ctrlKey, altKey, shiftKey}) => {
+  if (ctrlKey && altKey && code=='KeyD') {
+    body.classList.toggle('dark-theme')
+    if (body.classList.contains('dark-theme')) {
+      localStorage.darkTheme = true
+    } else {
+      delete localStorage.darkTheme
+    }
+  }
 }
