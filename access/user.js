b4d4c83bd3cd1eda75d1ae9ba9ac0f0fa98320ce
unibreakfast <unibreakfast1@gmail.com>
2020-11-22 11:59:06 +0200

    show users in the table

--- access/user.js
+++ access/user.js
@@ -1,8 +1,10 @@
 module.exports = ({request, response, users}) => {
   const {token} = request.cookie
   if (!token) return
+
   const user = users.find(user => user.token == token)
   if (!user) return
+
   response.setCookie('token', token)
   return user
 }
--- public/js/users.js
+++ public/js/users.js
@@ -1,4 +1,14 @@
-function repeatUsers(num) {
-  const tbody = usersTable.tBodies[0]
-  tbody.innerHTML = tbody.innerHTML.repeat(num)
+getUsers()
+
+
+async function getUsers() {
+  const response = await fetch('/api/users')
+  if (!response.ok) return goTo('/', 'up')
+
+  const users = await response.json()
+  userTable.tBodies[0].innerHTML = users.map(buildRow).join('')
+}
+
+function buildRow(user) {
+  return `<tr><td>${user.login}</td><td>${user.name}</td></tr>`
 }
