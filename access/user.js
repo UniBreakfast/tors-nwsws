8142d43c7ae933fdfcaef0c6cbac7bd8e5416416
unibreakfast <unibreakfast1@gmail.com>
2020-11-22 17:56:03 +0200

    c users

--- access/user.js
+++ access/user.js
@@ -1,10 +1,12 @@
-module.exports = ({request, response, users}) => {
-  const {token} = request.cookie
-  if (!token) return
+const { c } = require("node-web-server-with-stuff")
 
+module.exports = ({request, response, users}) => { c('users', users)
+  const {token} = request.cookie.c()
+  if (!token) return
+c('token')
   const user = users.find(user => user.token == token)
   if (!user) return response.delCookie('token')
-
+c('user')
   response.setCookie('token', token)
   return user
 }
