f06848152ce461ea6d107d8e13b5c69318b4e0c4
unibreakfast <unibreakfast1@gmail.com>
2020-11-22 16:59:04 +0200

    testing

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { c } = require('node-web-server-with-stuff')
+
 global.users = [
   {login: 'admin', name: 'Administrator', password: 'qwerty'},
   {login: 'bob', name: 'Robert', password:'asdf'},
 ]
 
+const { server, c } = require('node-web-server-with-stuff')
 
-require('node-web-server-with-stuff').server.run({given: {users}})
+c('index')
+
+server.run({given: {users}})
