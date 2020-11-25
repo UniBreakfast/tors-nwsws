34cac2eb5b347d60c961aac1ddb506b522150d8e
unibreakfast <unibreakfast1@gmail.com>
2020-11-25 16:33:23 +0200

    remove global users array and db shortcut

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ try { require('./stuff.cjs') }
 catch { console.warn("No stuff.cjs found in root. It's ok if you have all appropriate system variables set. Otherwise you supposed to create a stuff.сjs by the stuff_template.cjs example you have in root.") }
 
 
-global.users = [
-  {login: 'admin', name: 'Administrator', password: 'qwerty'},
-  {login: 'bob', name: 'Robert', password:'asdf'},
-]
-
 const { server, c } = require('node-web-server-with-stuff')
 const {hash, verify} = require('./hashVerify.cjs')
 const {connect, ObjectId} = require('./useMongo.cjs')
@@ -17,4 +12,4 @@ const given = {users, hash, verify, db, ObjectId}
 server.run({given})
 
 
-db.then(db => global.db = db)
+// db.then(db => global.db = db)
